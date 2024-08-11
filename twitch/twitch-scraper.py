9a03d943fdf998a6c71f5ee8caf7acd4a1cb54d8
UniversalGeneve1 <puzonralph@gmail.com>
2024-08-11 01:05:07 -0700

    updated writing mechanism to NAS

--- twitch/twitch-scraper.py
+++ twitch/twitch-scraper.py
@@ -1,17 +1,24 @@
 from chat_downloader import ChatDownloader
 from datetime import datetime
+from distutils.dir_util import copy_tree
 from time import sleep
 from twitch_live_sensor import twitch_sensor_check
 import json, os, random, sys
 
-streamer = sys.argv[1] 
-url = f"https://www.twitch.tv/{streamer.lower()}"
+
+streamer = (sys.argv[1]).lower() 
+url = f"https://www.twitch.tv/{streamer}"
+
+
+streamer_data = f"data/{streamer}"
+streamer_store = f"/mnt/NASpipe/twitch/data/{streamer}"
+
 
 ### main:
 # start live listener
-# once live, generate files then start scrape, timeout will handle termination of scrape
+# once live, generate files then start scrape, 
+#   timeout will handle termination of scrape
 # write to nas
-
 while twitch_sensor_check(url) == False:
 	print(f"{streamer} not live")
 	sleep((29 + random.uniform(0, 5)))
@@ -19,14 +26,18 @@ while twitch_sensor_check(url) == False:
 	
 print(f"{streamer} has gone live! Starting scrape")
 
+
 todays_date = datetime.now().strftime('%Y-%m-%d')
 time_of_trigger = datetime.now().strftime('%H%M%S')
 streamer_dir_root = f"data/{streamer}"
+
 
 #make dirs and file
 os.makedirs(f"{streamer_dir_root}/preprocessed", exist_ok=True)
 os.makedirs(f"{streamer_dir_root}/insights", exist_ok=True)
 
+
+#handle jsonl files with pandas.read_json(..., lines=True)
 filename = f"{streamer_dir_root}/raw/RAW_{streamer}_{todays_date}_{time_of_trigger}.jsonl"
 os.makedirs(os.path.dirname(filename), exist_ok=True)
 
@@ -37,15 +48,10 @@ with open(filename, 'a+') as f:
 		chat.print_formatted(message) #terminal check
 	print(f"{streamer} is offline, done scraping")
 	
-#write file to raw WAITING FOR NAS
-print("broke out of scrape test, write file to NAS here")
 
- 
-"""
-this script generates a jsonl file, handle it like this:
+print(f"scrape complete, writing {streamer}'s data into NAS:")
 
-	import pandas as pd    
-	jsonObj = pd.read_json(path_or_buf=file_path, lines=True)
+streamer_data = f"data/{streamer}"
+streamer_store = f"/mnt/NASpipe/twitch/data/{streamer}"
+copy_tree(streamer_data, streamer_store)
 
-note the `lines=True` parameter, this handles jsonl
-"""
