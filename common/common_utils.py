fc27a85606c0fbc5f0ee3a42103ff7f670584c6d
UniversalGeneve1 <puzonralph@gmail.com>
2024-09-28 00:16:26 -0700

    refactored preprocessor file, turned read files to process as a commom util

--- common/common_utils.py
+++ common/common_utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os, shutil
 
 stages = ['raw', 'preprocessed', 'insights']
+nas_base_dir = "/mnt/NASpipe"	
 
 #helper:
 def init_file(fp):
@@ -11,7 +12,6 @@ def init_file(fp):
 	file_path.touch(exist_ok=True)
 
 def init_streamer_files(streamer, stages=stages):
-	
 	todays_date = datetime.now().strftime('%Y-%m-%d')
 	time_of_trigger = datetime.now().strftime('%H%M%S')
 	streamer_dir_root = f"data/{streamer}"
@@ -21,36 +21,87 @@ def init_streamer_files(streamer, stages=stages):
 	raw_fn= f"{streamer_dir_root}/raw/RAW_{streamer}_{todays_date}_{time_of_trigger}.jsonl"
 	return raw_fn
 	
-
-def write_to_store(platform, streamer, data, write_stage, stages=stages):
 	
-	#ensure dirs exist in NAS:
+def ensure_dirs(write_stage, platform, streamer, nas_base_dir=nas_base_dir, stages=stages):
 	if write_stage.lower() not in stages:
 		raise ValueError("invalid write stage")
-	
-	nas_base_dir = "/mnt/NASpipe"	
+		
 	[os.makedirs(f"{nas_base_dir}/{platform}/{streamer}/{stage}", exist_ok=True) for stage in stages]
+	
+	
+	
+def write_to_store(platform, streamer, data, write_stage, stages=stages):
+	
+	ensure_dirs(write_stage, platform, streamer)
 
-	#tracking file:
+	# tracking file:
 	trk_file = f"data/{streamer}/{write_stage}/tracking.txt"
 	
 	# point to NAS in appropriate level
 	destination = f"{nas_base_dir}/{platform}/{streamer}/{write_stage}"
 	
-	#copy file into NAS
+	# copy file into NAS
 	try:
 		shutil.copy(data, destination)
 		print(f"{data} moved successfully to {destination}")
 	except Exception as e:
 		print(f"Error moving file: {e}")
 		
-	#add filename into tracking file:
+	# add filename into tracking file:
 	fname = data.rsplit("/", 1)[1]
 	with open(f"data/{streamer}/{write_stage}/tracking.txt", "a+") as f:
 		f.write(fname)
 	
+	# write tracking file into the storage
 	try:
 		shutil.copy(trk_file, destination)
 		print(f"tracking file updated on NAS")
 	except Exception as e:
 		print(f"Error moving tracking file: {e}")
+		
+
+def read_files_to_process(input_stg, streamer, stages=stages):
+	"""
+		When a filename is written on its stage's tracking file, it has
+		been processed 
+	"""
+	ran_files = []
+	
+	with open(f"data/{streamer}/{input_stg}/tracking.txt", 'r') as ptf:
+		for ln in ptf:
+			ran_files.append(ln)
+
+	return [f for f in os.listdir(f"data/{streamer}/{input_stg}") if f not in ran_files and f!= 'tracking.txt']
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- twitch/twitch-preprocessor.py
+++ twitch/twitch-preprocessor.py
@@ -4,10 +4,9 @@ import csv, json, sys
 import pandas as pd
 
 sys.path.append('../common')
-from common_utils import write_to_store
+from common_utils import write_to_store, read_files_to_process
 
 streamer = (sys.argv[1]).lower() 
-base_dir = f"data/{streamer}/preprocessed"
 
 bot_names = [
 	'nightbot',
@@ -22,9 +21,9 @@ bot_names = [
 	'songlistbot',
 	'irltoolkit',
 	'lolrankbot',
-	'soundalerts'
+	'soundalerts',
+	'frostytoolsdotcom'
 ]
-
 col_names = [
 	'timestamp',
 	'action_type',
@@ -38,7 +37,7 @@ col_names = [
 	'is_moderator', #author
 	'is_subscriber',
 	'is_turbo',
-	'badge1_name', #author - badges. users have max 4 badges
+	'badge1_name', #author['badges']. users have max 4 badges
 	'badge1_title',
 	'badge2_name',
 	'badge2_title',
@@ -47,7 +46,7 @@ col_names = [
 	'badge4_name',
 	'badge4_title'
 ]
-
+#HELPERS:
 def data_impute(obj, key):
 	if key in obj:
 		return obj[key]
@@ -65,22 +64,13 @@ def process_twitch_user_badge_data(badges):
 	return badge_data
 
 
-###
-
-preproc_files_tracking = f"{base_dir}/tracking.txt"
-
-ran_files = []
-
-with open(preproc_files_tracking, 'r') as tf:
-	for ln in tf:
-		ran_files.append(ln)
-
-files_to_run = [f for f in os.listdir(f"data/{streamer}/raw") if f not in ran_files and f!= 'tracking.txt']
+### MAIN:
+files_to_run = read_files_to_process("raw", streamer)
 
 for f2r in files_to_run: 
 	file_loc = f"data/{streamer}/raw/{f2r}"
 	file_id_name = "PREPROC_" + f2r.split(".")[0][4:]
-	landing_file = f"{base_dir}/{file_id_name}.csv"
+	landing_file = f"data/{streamer}/preprocessed/{file_id_name}.csv"
 
 	data = []	
 	with open(file_loc, 'r') as file:
@@ -89,8 +79,8 @@ for f2r in files_to_run:
 			json_object = json.loads(line.strip())
 			if (json_object['author']['display_name']).lower() in bot_names:
 				continue
-			[row.append(data_impute(json_object, x)) for x in col_names[:9]] #first 10 cols are top level
-			[row.append(data_impute(json_object['author'], x)) for x in col_names[9:12]] # cols 11-13 are in author object
+			[row.append(data_impute(json_object, x)) for x in col_names[:9]] #first 9 cols are top level
+			[row.append(data_impute(json_object['author'], x)) for x in col_names[9:12]] # cols 10-12 are in author object
 			row = row + process_twitch_user_badge_data(json_object['author'])
 			data.append(row)
 			
